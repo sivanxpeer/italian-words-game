9b57fbdc01f3eeda5c2c880182ee895f54f1473d
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-30 20:04:53 +0200

    Mid-Project start

---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            {"Main:"}
+            <br></br>
+            {"this component will be the main page of the game - users dash board , choose a category to learn, hold state and data on user"}
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../Main/Main';
+
+const Welcome = () => {
+    return (
+        <div>
+            {"Welcome Page:"}<br></br>
+            {"this page will have a login with google api a welcome message"}
+            <Main></Main>
+        </div>
+    )
+}
+
+export default Welcome;
