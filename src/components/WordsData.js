a2e9176abfac76fd69321356d97b7a91df4bcdf4
sivanxpeer <sivan.x.peer@gmail.com>
2021-12-31 15:50:42 +0200

    getting data from translation api

--- src/components/WordsData.js
+++ src/components/WordsData.js
@@ -1,24 +1,41 @@
-    
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 //this is the api for translating 
 
 
-export default function WordsData(){
-const options = {
-  method: 'GET',
-  url: 'https://nlp-translation.p.rapidapi.com/v1/translate',
-  params: {text: 'ladder', to: 'it', from: 'en'},
-  headers: {
-    'x-rapidapi-host': 'nlp-translation.p.rapidapi.com',
-    'x-rapidapi-key': 'd40f514ce8msh502a22fb9b4072bp110b64jsn102955aa68b4'
-  }
-};
+const WordsData = () => {
+    const arr = ["father", "mother", "sister", "brother"];
+    //method to get a word in a language and 5 words in english, (one of them is the right translation)
+    const [myData, setMyData] = useState(null);
+    // useEffect(()=>{counter+=1},[myData]) //equal to componentDidmount
+    const [counter,setCounter]=useState(0);
+    const getData = async (textParam) => {
+        try {
+            if(arr[counter])
+            {
+                const { data } = await axios.get("https://nlp-translation.p.rapidapi.com/v1/translate", {
+                    params: { text: textParam, to: 'it', from: 'en' },
+                    headers: {
+                        'x-rapidapi-host': 'nlp-translation.p.rapidapi.com',
+                        'x-rapidapi-key': 'd40f514ce8msh502a22fb9b4072bp110b64jsn102955aa68b4'
+                    }
+                })
+                setMyData(data);
+                setCounter(prevState=>prevState+1);
+                console.log(data.original_text, data.translated_text);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
 
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-});
+    }
 
-    return(<div>Hi</div>)
+    return (
+        <div>
+            <button onClick={() => getData(arr[counter])}>Get data</button>
+
+            <div>{myData && myData.original_text}</div>
+        </div>
+    );
 }
+export default WordsData;
