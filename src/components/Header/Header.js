ade50b6578a9179f52347531b697c993440c9b88
sivanxpeer <sivan.x.peer@gmail.com>
2022-01-03 04:26:21 +0200

    function to get an italian word

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,36 +3,42 @@ import './Header.css'
 import api from '../../Api'
 const Header = () => {
     const [data, setData] = useState();
-// const [itWords,setItWords] = useState();
+    const [itWords, setItWords] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
             const d = await api.getItems();
-            console.log(d);
+            console.log("data:",data[1].italian); //get 1 italian word
             setData(d);
+            // setItWords(prevWords=>[...prevWords],data['italian']);
+            // console.log("italian: "+itWords);x
+            
         };
-
+        
         fetchData();
     }, []);
 
-    const displayWords = (elements) => {
-        const mappedElements =elements.map((word) => {
-            return (<div>
-            {word.italian}
-        </div>
-         ) })
-        return mappedElements
-    }
-    
+    // console.log("hiiii", data)
+    // const displayWords = (elements) => {
+    //     const mappedElements =elements.map((word) => {
+    //         return (<div>
+    //         {word.italian}
+    //     </div>
+    //      ) })
+    //     return mappedElements
+    // }
 
-return (
-    <div className="header">
-        <span className="title">BiLinguale 🌏</span>
-        {/* <button onClick={()=>displayWords(data.italian)}>get italian words</button> */}
-    
-        {/* <div>{()=>displayWords()}</div> */}
-        {/* <div className="inputs"></div> */}
-    </div>
-)
+
+    return (
+        <div className="header">
+            <span className="title">BiLinguale 🌏</span>
+            {/* <button onClick={()=>displayWords(data.italian)}>get italian words</button> */}
+            <div>
+                
+            </div>
+            {/* <div>{()=>displayWords()}</div> */}
+            {/* <div className="inputs"></div> */}
+        </div>
+    )
 }
 
 export default Header
