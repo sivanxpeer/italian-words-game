e7e8cfc4d47b3fbb073bae6d83004ed324e5885c
sivanxpeer <sivan.x.peer@gmail.com>
2022-06-14 13:04:14 +0300

    redirect to game when clicking start after login

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-// import { BrowserRouter, Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 // import GameCard from '../GameCard/GameCard';
 import './Header.css'
 const Header = ({ myUser }) => {
     const [btn, setbtn] = useState("false");
+    const history = useHistory();
 
     const start = () => {
         console.log("start");
         if (!myUser && !btn) {
+            history.push('/gamecard');
+            
             disp();
             setbtn(prevState => !prevState);
             // setbtn(false);
